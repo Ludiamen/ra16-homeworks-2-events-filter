9abf718bf4cf536cda2faa7c851597bb5b4f8870
Ludiamen <maxvas1001@gmail.com>
2024-09-18 00:47:34 +0300

    initial commit

---
+++ src/Components/ProjectList/ProjectList.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+export const ProjectList = (props) => {
+    const pictures = props.projects.map((project, i) => <li className="picture" key={i}>
+    <img src={project.img}></img></li>)
+  return (
+    <ul className="pictures">
+        {pictures}
+    </ul>
+  )
+}
---
+++ src/Components/Toolbar/Toolbar.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import { Component } from 'react'
+
+export class Toolbar extends Component {
+
+  render() {
+    return (
+      <div className='filters'>
+        {this.props.filters.map((filter, i) => filter === this.props.selected ? 
+        <button className='filter selected' key={i} onClick={() => this.props.onSelectedFilter(filter)}>{filter}</button> : 
+        <button className='filter' key={i} onClick={() => this.props.onSelectedFilter(filter)}>{filter}</button>)}
+      </div>
+    )
+  }
+}
